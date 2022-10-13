56834c7dad6dbcaeb5e81413e236004c8676a733
HP <sarvartulkunov02@gmail.com>
2022-10-13 09:49:39 +0500

    Pointcuts and Wildcard Expressions

--- src/main/java/org/example/AopMain.java
+++ src/main/java/org/example/AopMain.java
@@ -8,6 +8,6 @@ public class AopMain {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         ShapeService shapeService = context.getBean("shapeService", ShapeService.class);
-        System.out.println(shapeService.getCircle().getName());
+        System.out.println(shapeService.getTriangle().getName());
     }
 }
--- src/main/java/org/example/aspects/LoggingAspect.java
+++ src/main/java/org/example/aspects/LoggingAspect.java
@@ -2,13 +2,24 @@ package org.example.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(public String getName())")
+
+    @Pointcut("execution(* get*())")
+    public void allGetters(){};
+
+    @Before("allGetters()")
     public void loggingAdvice(){
         System.out.println("Advice run. Get Method Called");
     }
+
+    @Before("allGetters()")
+    public void secondLoggingAdvice(){
+        System.out.println("Second Advice executed");
+    }
+
 
 }
