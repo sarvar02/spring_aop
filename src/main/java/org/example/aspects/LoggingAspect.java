a688f67db0ed4a0f834664fe9f243538b27e7af5
HP <sarvartulkunov02@gmail.com>
2022-10-14 09:44:44 +0500

    JoinPoints and Advice Arguments

--- src/main/java/org/example/aspects/LoggingAspect.java
+++ src/main/java/org/example/aspects/LoggingAspect.java
@@ -1,8 +1,10 @@
 package org.example.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.example.model.Circle;
 
 @Aspect
 public class LoggingAspect {
@@ -11,14 +13,25 @@ public class LoggingAspect {
     @Pointcut("execution(* get*())")
     public void allGetters(){};
 
-    @Before("allGetters()")
-    public void loggingAdvice(){
-        System.out.println("Advice run. Get Method Called");
+    @Pointcut("within(org.example.model.Circle)")
+    public void allCircleMethods(){};
+
+
+//    @Before("args(String)")
+//    public void stringArgumentMethods(){
+//        System.out.println("A method that takes String arguments has been called");
+//    }
+
+    @Before("args(name)")
+    public void stringArgumentMethods(String name){
+        System.out.println("A method that takes String arguments has been called. The value is " + name);
     }
 
-    @Before("allGetters()")
-    public void secondLoggingAdvice(){
-        System.out.println("Second Advice executed");
+    @Before("allCircleMethods()")
+    public void loggingAdvice(JoinPoint joinPoint){
+        // getTarget() returns the current object
+        // Circle circle = (Circle) joinPoint.getTarget();
+
     }
 
 
